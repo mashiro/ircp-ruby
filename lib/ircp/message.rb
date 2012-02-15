1d3aba8b7c9457dc29f7417de550bc7cba2a6500
mashiro <mail@mashiro.org>
2012-02-15 18:41:02 +0900

    Add to_s and inspect override

--- lib/ircp/message.rb
+++ lib/ircp/message.rb
@@ -7,9 +7,25 @@ module Ircp
     def initialize(*args)
       options = args.last.is_a?(Hash) ? args.pop : {}
       @raw = options[:raw]
-      @prefix = options[:prefix].is_a?(Hash) ? Prefix.new(options[:prefix]) : nil
+      @prefix = Prefix.new(options[:prefix] || {})
       @command = options[:command]
       @params = args
+    end
+
+    def inspect
+      variables = instance_variables.map { |name| "#{name.inspect}=#{instance_variable_get(name).inspect}" }
+      variables.unshift "#{self.class}"
+      "<#{variables.join ' '}>"
+    end
+
+    def to_s
+      return @raw unless @raw.nil?
+
+      tokens = []
+      tokens.push @prefix unless @prefix.empty?
+      tokens.push @command unless @command.nil?
+      tokens.push *@params
+      tokens.join ' '
     end
   end
 end
--- lib/ircp/prefix.rb
+++ lib/ircp/prefix.rb
@@ -9,5 +9,29 @@ module Ircp
       @user = options[:user]
       @host = options[:host]
     end
+
+    def empty?
+      to_s.empty?
+    end
+
+    def inspect
+      variables = instance_variables.map { |name| "#{name.inspect}=#{instance_variable_get(name).inspect}" }
+      variables.unshift "#{self.class}"
+      "<#{variables.join ' '}>"
+    end
+
+    def to_s
+      return @raw unless @raw.nil?
+
+      if @servername
+        ":#{servername}"
+      else
+        tokens = []
+        tokens.push ":#{@nick}" unless @nick.nil?
+        tokens.push "!#{@user}" unless @user.nil?
+        tokens.push "@#{@host}" unless @host.nil?
+        tokens.join ''
+      end
+    end
   end
 end
