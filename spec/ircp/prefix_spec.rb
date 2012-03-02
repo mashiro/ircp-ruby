8bcc16c416727bb9742149c5b0cfa563d9be8932
mashiro <mail@mashiro.org>
2012-03-02 17:56:06 +0900

    Add prefix spec

---
+++ spec/ircp/prefix_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Ircp::Prefix do
+  describe '#initialize' do
+    context 'with servername' do
+      before { @prefix = Ircp::Prefix.new(:servername => 'example.com') }
+      subject { @prefix }
+      its(:servername) { should eq 'example.com' }
+    end
+
+    context 'with nick, user and host' do
+      before { @prefix = Ircp::Prefix.new(:nick => 'foo', :user => 'bar', :host => 'example.com') }
+      subject { @prefix }
+      its(:nick) { should eq 'foo' }
+      its(:user) { should eq 'bar' }
+      its(:host) { should eq 'example.com' }
+    end
+  end
+
+  describe '#to_s' do
+    context 'with servername' do
+      before { @prefix = Ircp::Prefix.new(:servername => 'example.com') }
+      subject { @prefix }
+      its(:to_s) { should eq ':example.com' }
+    end
+
+    context 'with nick, user and host' do
+      before { @prefix = Ircp::Prefix.new(:nick => 'foo', :user => 'bar', :host => 'example.com') }
+      subject { @prefix }
+      its(:to_s) { should eq ':foo!bar@example.com' }
+    end
+  end
+end
